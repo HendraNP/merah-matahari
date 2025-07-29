cf4f3d03c1eff19d8c2dd07e1bfc40df82a7397f
Hendra <hendra.putra002@yahoo.com>
2025-07-29 10:50:37 +0700

    update json-ld #5

--- resources/views/partials/productdetail-jsonld.blade.php
+++ resources/views/partials/productdetail-jsonld.blade.php
@@ -1,15 +1,18 @@
+@php
+    $jsonLd = [
+        "@context" => "https://schema.org/",
+        "@type" => "Product",
+        "name" => $product->product_name,
+        "image" => [asset($product->image)],
+        "description" => nl2br($product['description']),
+        "brand" => [
+            "@type" => "Brand",
+            "name" => $product->product_brand ?? 'Unknown Brand'
+        ]
+    ];
+@endphp
+
 <script type="application/ld+json">
-{
-  "@context": "https://schema.org/",
-  "@type": "Product",
-  "name": "{{ $product->product_name }}",
-  "image": [
-    "{{ asset($product->image) }}"
-  ],
-  "description": {!! json_encode(nl2br($product['description'])) !!},
-  "brand": {
-    "@type": "Brand",
-    "name": "{{ $product->product_brand ?? 'Unknown Brand' }}"
-  }
-}
+{!! json_encode($jsonLd, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
 </script>
+{{-- This JSON-LD script provides structured data for the product detail page, enhancing SEO and enabling rich snippets in search results. --}}
