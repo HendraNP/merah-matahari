e8d08c7c03a9788b37019131f0cc41ef0fe3e46b
Hendra <hendra.putra002@yahoo.com>
2025-07-29 09:28:40 +0700

    remove json-ld

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,33 +8,6 @@
     <meta name="keywords" content="cat, supplier cat, manufaktur cat, cat dinding, cat epoxy, cat anti korosi">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
-    <script type="application/ld+json">
-    {
-    "@context": "https://schema.org",
-    "@type": "Organization",
-    "name": "Merah Matahari",
-    "url": "https://merahmatahari.co.id",
-    "logo": "https://merahmatahari.co.id/logo.png",
-    "sameAs": [
-        "https://www.instagram.com/officialmmpaint",
-        "https://www.tiktok.com/@mmpaint",
-        "https://id.shp.ee/fiQypk8"
-    ],
-    "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "Kawasan Industri Jababeka 1 Jl. Jababeka XV 11B, Blok VI No. U19 J",
-        "addressLocality": "Cikarang",
-        "addressRegion": "Jawa Barat",
-        "postalCode": "17530",
-        "addressCountry": "ID"
-    },
-    "contactPoint": {
-        "@type": "ContactPoint",
-        "telephone": "+6221-50300308",
-        "contactType": "Marketing"
-    }
-    }
-    </script>
 
     @yield('jsonld')
 </head>
