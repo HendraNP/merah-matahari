4571b6c11f9c3d5ea1455ff52c10faaaa8932b90
Hendra <hendra.putra002@yahoo.com>
2025-07-29 11:40:33 +0700

    add more keywords

--- resources/views/partials/productdetail-jsonld.blade.php
+++ resources/views/partials/productdetail-jsonld.blade.php
@@ -16,7 +16,7 @@
     {
       "@type": "PropertyValue",
       "name": "Keywords",
-      "value": ['cat', 'cat dinding', 'cat tembok', 'cat epoxy', 'cat anti korosi', 'supplier cat', 'manufaktur cat', 'cat berkualitas', 'cat warna', 'cat dinding terbaik', 'cat epoxy terbaik', 'cat anti korosi terbaik']
+      "value": ['cat', 'cat dinding', 'cat tembok', 'cat epoxy', 'cat anti korosi', 'supplier cat', 'manufaktur cat', 'cat berkualitas', 'cat warna', 'cat dinding terbaik', 'cat epoxy terbaik', 'cat anti korosi terbaik', 'cat tembok eksterior', 'cat tembok interior', 'cat dinding eksterior', 'cat dinding interior', 'cat dinding murah', 'cat dinding berkualitas', 'cat epoxy murah', 'cat epoxy berkualitas', 'cat anti korosi murah', 'cat anti korosi berkualitas','cat tembok tahan lama', 'cat epoxy tahan lama', 'cat anti korosi tahan lama', 'cat dinding tahan lama', 'cat tembok berkualitas tinggi','cat dinding berkualitas tinggi', 'cat epoxy berkualitas tinggi', 'cat anti korosi berkualitas tinggi','beli cat tembok online', 'beli cat epoxy online', 'beli cat anti korosi online', 'jual cat dinding', 'jual cat epoxy', 'jual cat anti korosi', 'harga cat tembok terbaru', 'harga cat epoxy terbaru', 'harga cat anti korosi terbaru', 'cat dinding harga terjangkau', 'cat epoxy harga terjangkau', 'cat anti korosi harga terjangkau' ,'cat dinding harga murah', 'cat epoxy harga murah', 'cat anti korosi harga murah', 'cat dinding untuk interior', 'cat dinding untuk eksterior', 'cat epoxy untuk lantai', 'cat anti korosi untuk logam', 'cat tembok Nippon / Dulux / Avian']
     }
   ]
 }
